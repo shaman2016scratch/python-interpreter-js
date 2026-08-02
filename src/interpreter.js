4048433954d16c8bb0de41693b1f60c1defcd43e
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-02 21:43:24 +0300

    Update interpreter.js

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,3 +1,15 @@
-class interpreter {}
+class interpreter {
+  constructor (py) {
+    this.python = py
+  }
+
+  interpretation () {
+    const code = py
+    let interpreterData = {
+      line: 0,
+      symbol: 0
+    }
+  }
+}
 
 export default interpreter
