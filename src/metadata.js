9c3f5f26faf2f70ce0641e8890fbb2052e907fd1
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-03 15:52:16 +0300

    Update metadata.js

--- src/metadata.js
+++ src/metadata.js
@@ -2,7 +2,10 @@ const metadata = {
   name: "python-interpreter-js",
   version: "0.0.0",
   license: "GPL-3.0",
-  author: "polzovatel_8787"
+  author: "polzovatel_8787",
+  links: {
+    source_code: "https://github.com/shaman2016scratch/python-interpreter-js"
+  }
 }
 
 export default metadata
