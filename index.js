42579392cde9c2155d14ca61f2b0645c0c0e8fef
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-03 21:43:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import interpreter from "./src/interpreter.js"
+import Import from "./src/import.js"
 
 class PythonInterpreter {
-  constructor () {}
+  constructor (code, type, imports) {
+    this.code = code
+    this.type = type
+    this.imports = imports
+  }
+
+  async runAsEval () {
+    const result = new interpreter(this.code)
+    await result.interprete()
+    result.eval()
+  }
 }
 
 export default PythonInterpreter
