0eb837e2e46f362b26efc4ed6aee14b71aff8eee
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-03 21:50:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ class PythonInterpreter {
 
   async runAsEval () {
     const result = new interpreter(this.code)
-    await result.interprete()
+    await result.interpretation()
     result.eval()
+  }
+
+  async runAsFunction () {
+    const result = new interpreter(this.code)
+    await result.interpretation()
+    result.func()
+  }
+
+  async interpretation () {
+    const result = new interpreter(this.code)
+    await result.interpretation()
+    return result.generated
   }
 }
 
