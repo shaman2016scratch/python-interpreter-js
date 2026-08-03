7ea903b16a1b9d23c5b40cf32eb69c4790ead947
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-03 21:13:13 +0300

    Update replaces.js

--- src/replaces.js
+++ src/replaces.js
@@ -1,7 +1,9 @@
 const replaces = {
   "print": "console.log('{content}')",
   "defStart": "variables.{name} = function ({args}) {",
-  "defEnd": "}; variablesData.{name} = { type: types.str }"
+  "defEnd": "}; variablesData.{name} = { type: types.function };",
+  "input": "console.error('input is not defined')",
+  "exit": "process.exit()"
 }
 
 export default replaces
