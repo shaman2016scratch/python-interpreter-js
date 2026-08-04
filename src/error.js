d0a8f5b0a63067369280b176e3bf017281da22d4
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-04 11:14:19 +0300

    Update error.js

--- src/error.js
+++ src/error.js
@@ -4,8 +4,28 @@ class Error {
     this.m = metadata
   }
 
-  syntax (a) {
+  syntax () {
     return `Syntax Error: ${this.text} in ${this.m.line}, ${this.m.symbol}`
+  }
+
+  type () {
+    return `Type Error: ${this.text} in ${this.m.line}, ${this.m.symbol}`
+  }
+
+  name () {
+    return `Name Error: ${this.text} in ${this.m.line}, ${this.m.symbol}`
+  }
+
+  other () {
+    return `Error: ${this.text} in ${this.m.line}, ${this.m.symbol}`
+  }
+
+  setType (t) {
+    return `${t} Error: ${this.text} in ${this.m.line}, ${this.m.symbol}`
+  }
+
+  noType () {
+    return `${this.text} in ${this.m.line}, ${this.m.symbol}`
   }
 }
 
